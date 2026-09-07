1879fbe5a71b35089f8d165c970cad54ad3e5248
sharjeelwakeel <82971599+sharjeelwakeel@users.noreply.github.com>
2026-09-07 09:54:38 -0700

    removing scratch folder

--- scratch/check_acc.php
+++
@@ -1,6 +0,0 @@
-<?php
-require 'include/config.php';
-$res = mysqli_query($connection, "SELECT id, slip_no, slip_type, account_number, vehicle_number, quantity, wasoli, temp_slip_id, temp_slip_no FROM tbl_meter_reading_credit_sales WHERE id IN (78, 81, 82)");
-while ($r = mysqli_fetch_assoc($res)) {
-    echo json_encode($r) . "\n";
-}
